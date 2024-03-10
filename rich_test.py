6a049258c6cc8aaaec1f4b84dd60b750e7dfcecf
Abhishek Rathore <z.abhirathore@gmail.com>
2024-03-10 19:19:45 +0530

    Some files deleted

--- rich_test.py
+++
@@ -1,7 +0,0 @@
-from rich import print
-from rich.layout import Layout
-
-l1 = Layout(name="Main")
-l1.split_column(Layout(name="Upper"),Layout(name="Lower"))
-l1['Lower'].split_row(Layout(name="Left"),Layout(name="Right"))
-print(l1)
